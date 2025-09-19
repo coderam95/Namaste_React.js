e928a88f0c03efd6f1c6916c11460dd8325f8c66
Hariram N <hariramn@Harirams-MacBook-Air.local>
2025-09-19 11:36:14 +0530

    restaurant cards dynamic

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import restaurantData from './restaurants.json';
 
 const parent = React.createElement('div', {id: 'parent'}, React.createElement('div', {id: 'child'}, 
     [React.createElement('h1', {id: 'h1'}, 'I am a h1 tag'), React.createElement('h2', {id: 'h2'}, 'I am an h2 tag')]));
@@ -9,13 +9,6 @@ const parent = React.createElement('div', {id: 'parent'}, React.createElement('d
 const heading = React.createElement('h1', {id: 'heading', xyz: 'abc'}, 'Hello World from React.js');
 console.log(parent, 'parent');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-
-
-
-
-
 
 const Header = () => {
     return(
@@ -36,16 +29,22 @@ const Header = () => {
 }
 
 
-const RestoCard = () => {
+const mediaLink = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
+
+const RestoCard = (props) => {
+    console.log(props,'props');
+    const {name, cuisines, avgRating, cloudinaryImageId} = props.resList.info;
     return(
     <div className='restoCard'>
         <img 
         alt='restoImage' 
-        className='restoImage' src='https://media-assets.swiggy.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/j6xodxgd7hukpzkdzzz4' />
-        <h3>Basil with a twist</h3>
-        <h4>Italian, Desserts</h4>
-        <h4>4.9 Stars</h4>
-        <h4>30 Mins</h4>
+        className='restoImage' 
+        src={mediaLink + cloudinaryImageId}
+        />
+        <h3>{name}</h3>
+        <h4>{cuisines.join(', ')}</h4>
+        <h4>{avgRating} Stars</h4>
+        <h4>{props.resList.info.sla.slaString}</h4>
     </div>
     )
 }
@@ -54,27 +53,17 @@ const Body = () => {
     return(
         <div className='body'>
             <div className='restoCardContainer'>
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-                <RestoCard />
-
+                {
+                restaurantData.restaurants.map((restaurant) => {
+                    return(
+                        <RestoCard key={restaurant.info.id} resList={restaurant} />
+                    )
+                })
+            }
             </div>
         </div>
     )
 }
-
-
-
-
 
 const AppLayout = () => {
     return(
