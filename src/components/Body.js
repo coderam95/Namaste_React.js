8b9afc64366b296821271b999174df46073684cb
Hariram N <hariramn@Harirams-MacBook-Air.local>
2025-10-09 11:05:56 +0530

    shimmer, api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,60 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantsData from '../utils/restaurants.json'
+import restaurantsData from "../utils/restaurants.json";
+import Shimmer from "./Shimmer";
+import { use, useEffect, useState } from "react";
 
 const Body = () => {
-    return(
-        <div className='body'>
-            <div className='search'>Search</div>
-            <div className='restaurantContainer'>
-                {
-                    restaurantsData.restaurants.map(restaurant => {
-                        return (
-                        <RestaurantCard key={restaurant.info.id} resList={restaurant} />
-                    )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+  const [listOfRestaurants, setListofRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.872589376447683&lng=80.20514437766997&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json, "json object");
+    //Optional chaining
+    setListofRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
+  // Local state variable - super powerful variable
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            //filter logic
+            setListofRestaurants();
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            setListofRestaurants(filteredList);
+          }}
+        >
+          Top rated restaurants
+        </button>
+      </div>
+      <div className="restaurantContainer">
+        {listOfRestaurants.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.info.id} resList={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
