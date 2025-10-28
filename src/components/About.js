0276afca109644aaee5748d61f58aa772e5163e8
Hariram N <hariramn@Harirams-MacBook-Air.local>
2025-10-28 16:38:43 +0530

    routing basics

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="About">
+      <h1>About</h1>
+      <h2>This is swiggy trial website</h2>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,28 @@
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return(
-        <div className='header'>
-            <div className='logoStyles'>
-                <img className='logoImage' alt='logo-photo' src={LOGO_URL} />
-            </div>
-            <div className='nav-items'>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="logoStyles">
+        <img className="logoImage" alt="logo-photo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
