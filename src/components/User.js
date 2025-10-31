20f7f52e181efb7c9a5c24e7eec1605edda5b506
Hariram N <hariramn@Harirams-MacBook-Air.local>
2025-10-31 11:18:59 +0530

    state in class components

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+
 const User = ({ name }) => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(1);
+
   return (
     <div className="user-card">
+      <h1>count = {count}</h1>
+      <h1>count2 = {count2}</h1>
       <h2>Name: {name} </h2>
       <h3>Location: Dehradun</h3>
       <h4>Contact: @akshaymarch7</h4>
