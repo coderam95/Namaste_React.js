a87a78f711abe6da881ddc9bb4a1fbf93434df0c
Hariram N <hariramn@Harirams-MacBook-Air.local>
2025-10-31 07:47:45 +0530

    class components

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div className="about">
       <h1>About</h1>
       <h2>This is Namaste React Web Series</h2>
+      <User name={"Akshay saini (function)"} />
+      <UserClass name={"Akshay Saini (class)"} location={"Dehradun (class)"} />
     </div>
   );
 };
