f23b61589b51a0d5fcfc074effb3ec7683fd6f93
Hariram N <hariramn@Harirams-MacBook-Air.local>
2025-10-31 11:23:56 +0530

    add state to class

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,12 @@
+import { use, useState } from "react";
+
 const User = ({ name, location }) => {
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(1);
   return (
     <div className="userCard">
+      <h1>count = {count}</h1>
+      <h1>count1 = {count1}</h1>
       <h2>Name: {name}</h2>
       <h3>Location: {location}</h3>
       <h4>User Name: coderam95</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,12 +3,20 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
 
   render() {
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="userCard">
+        <h1>Count : {count}</h1>
+        <h1>Count2 : {count2}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h3>UserName: coderam95</h3>
