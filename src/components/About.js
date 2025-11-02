675e617df873d48ddc888e27aba7de457f04c868
Hariram N <hariramn@Harirams-MacBook-Air.local>
2025-11-02 06:12:21 +0530

    class lifecycle

--- src/components/About.js
+++ src/components/About.js
@@ -4,21 +4,20 @@ import UserClass from "./UserClass";
 class About extends React.Component {
   constructor(props) {
     super(props);
-    console.log("Parent Constructor called");
+    //console.log("Parent Constructor called");
   }
 
   componentDidMount() {
-    console.log("Parent componentDidMount called");
+    //console.log("Parent componentDidMount called");
   }
 
   render() {
-    console.log("Parent render called");
+    //console.log("Parent render called");
     return (
       <div className="about">
         <h1>About</h1>
         <h2>This is Namaste React Web Series</h2>
         <UserClass name={"First(class)"} location={"Dehradun (class)"} />
-        <UserClass name={"Second (class)"} location={"Delhi (class)"} />
       </div>
     );
   }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,36 +5,45 @@ class UserClass extends React.Component {
     super(props);
 
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "blank",
+        location: "default",
+        avatar_url: "http://dummy-photo.com",
+      },
     };
-
     console.log(props);
     console.log(this.props.name + "Child Constructor called");
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     console.log(this.props.name + "Child componentDidMount called");
+
+    const data = await fetch("https://api.github.com/users/akshaymarch7");
+    const json = await data.json();
+
+    console.log(json, "JSON");
+
+    this.setState({ userInfo: json });
+  }
+
+  componentDidUpdate() {
+    console.log("child ComponentDidUpdate called");
+  }
+
+  componentWillUnmount() {
+    console.log("ComponentWillUnmount called");
   }
 
   render() {
-    console.log(this.props.name + "Child render called");
-    const { name, location } = this.props;
-    const { count } = this.state;
+    console.log("Child render");
+    const { name, location, login, avatar_url } = this.state.userInfo;
+    debugger;
     return (
       <div className="user-card">
-        <h1>Count = {count}</h1>
-        <button
-          onClick={() => {
-            this.setState({
-              count: count + 1,
-            });
-          }}
-        >
-          Count Increase
-        </button>
+        <img src={avatar_url} />
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
-        <h4>Contact: @akshaymarch7</h4>
+        <h4>Contact: {login}</h4>
       </div>
     );
   }
